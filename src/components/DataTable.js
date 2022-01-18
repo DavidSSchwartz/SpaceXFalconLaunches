f9056720ed0825a7f7baae67ad62c499b7b7ca24
David Schwartz <you@example.com>
2022-01-18 22:04:36 +0200

    set up table sorting

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -1,7 +1,34 @@
+import { useState } from 'react';
+
 const DataTable = ({ data }) => {
-    const rowData = data.map(item => {
+    const [ sortInfo, setSortInfo ] = useState(null);
+    
+    let sortedData = [...data];
+    
+    if (sortInfo !== null) {
+        sortedData.sort((a, b) => {
+            if (a[sortInfo.field] < b[sortInfo.field]) {
+                return sortInfo.direction === 'asc' ? -1 : 1;
+            }
+            if (a[sortInfo.field] > b[sortInfo.field]) {
+                return sortInfo.direction === 'asc' ? 1 : -1;
+            }
+            return 0;
+        });
+    }
+
+    const setUpSort = (field) => {
+        let direction = 'asc';
+        if (sortInfo?.field === field && sortInfo.direction === 'asc') {
+          direction = 'desc';
+        }
+        setSortInfo({ field, direction });
+    }
+
+
+    const rowData = sortedData.map(item => {
                         return (
-                            <tr>
+                            <tr key={item.id}>
                                 <td>{item.name}</td>
                                 <td>{item.date_local}</td>
                                 <td>{item.flight_number}</td>
@@ -10,15 +37,26 @@ const DataTable = ({ data }) => {
                             </tr>
                         )
                     });
+
     return (
         <table>
             <thead>
                 <tr>
-                    <th>Name</th>
-                    <th>Date</th>
-                    <th>Flight Number</th>
-                    <th>Success</th>
-                    <th>Details</th>
+                    <th onClick={() => setUpSort('name')}>
+                        Name
+                    </th>
+                    <th onClick={() => setUpSort('date_local')}>
+                        Date
+                    </th>
+                    <th onClick={() => setUpSort('flight_number')}>
+                        Flight Number
+                    </th>
+                    <th onClick={() => setUpSort('success')}>
+                        Success
+                    </th>
+                    <th>
+                        Details
+                    </th>
                 </tr>
             </thead>
             <tbody>
